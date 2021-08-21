e16b7fa097af2bc665040849f76af85c8f61ef5a
Sambit-12 <nayaksambit99@gmail.com>
2021-08-21 21:26:33 +0530

    sambit

---
+++ 3_Implementation/Main.c
@@ -0,0 +1,19 @@
+/**
+ * @file Main.c
+ * @author your name (Sambit.Nayak@ltts.com)
+ * @Finding the Duty Cycle and Output Voltage of Waveform by calculating the Time period at which Diode is on 
+ * and Hence Finding the Duty cycle after Finding Duty cycle we are giving Input voltage and Duty cycle value to find Output 
+ * Voltage of the waveform.
+ * @version 0.1
+ * @date 2021-08-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "DutyCycle.h"
+ int main(void)
+{
+     Sambit();
+     //Decleration of main function
+    return 0;
+}
